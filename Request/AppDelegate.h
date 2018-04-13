032d70467dbf00bee23ece7a51dd75fc94b9c027
YulncBd <zhd_dong@163.com>
2018-04-13 10:20:24 +0800

    Initial Commit

---
+++ Request/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Request
+//
+//  Created by DongZ on 2018/4/13.
+//  Copyright © 2018年 dz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
